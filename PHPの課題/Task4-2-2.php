d86facaa34cf2b676ca1647f089d3c52ad89e2e3
sakaguchi <songokusan318zin@gmail.com>
2023-06-16 22:50:10 +0900

    課題７の書き方変更
    &&を使用し省略する書き方に変更しました。

--- PHPの課題/Task4-2-2.php
+++ PHPの課題/Task4-2-2.php
@@ -9,26 +9,14 @@
   <?php
     $month = mt_rand(1,12);
 
-    if ($month === 3) {
-      $season = "春";
-    }elseif($month === 4) {
-      $season = "春";
-    }elseif ($month === 5) {
-      $season = "春";
-    }elseif ($month === 6) {
-      $season = "夏";
-    }elseif($month === 7) {
-      $season = "夏";
-    }elseif ($month === 8) {
-      $season = "夏";
-    }elseif ($month === 9) {
-      $season = "秋";
-    }elseif($month === 10) {
-      $season = "秋";
-    }elseif ($month === 11) {
-      $season = "秋";
-    }else {
-      $season = "冬";
+    if ($month >= 3 && $month <= 5) {
+      $season = '春';
+    } elseif ($month >= 6 && $month <= 8) {
+      $season = '夏';
+    } elseif ($month >= 9 && $month <= 11) {
+      $season = '秋';
+    } else {
+      $season = '冬';
     }
   ?>
 
