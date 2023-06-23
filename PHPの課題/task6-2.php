0072b3abfe8318d076b6a7b73f48538017a662da
sakaguchi <songokusan318zin@gmail.com>
2023-06-23 17:43:07 +0900

    課題12の修正
    課題12であるtask6-2.phpを修正しました。

--- PHPの課題/task6-2.php
+++ PHPの課題/task6-2.php
@@ -7,21 +7,38 @@
   </head>
   <body>
     <?php
-      function max_number ($n1,$n2){
-        if ($n1>$n2){
-          return $n1;           
-          }else {
-          return $n2;
-        }
-      }
+      // function max_number ($n1,$n2){
+      //   if ($n1>$n2){
+      //     return $n1;           
+      //     }else {
+      //     return $n2;
+      //   }
+      // }
 
+      // $a = 9;
+      // $b = 21;
+
+      // echo "\$a = $a<br>";
+      // echo "\$b = $b<br>";
+
+      // echo "\$aと\$bのうち最大値は" . max_number($a, $b) . "です。";
+
+
+      // echoを使用し、分かりやすい書き方に変更。
       $a = 9;
       $b = 21;
-
+      
       echo "\$a = $a<br>";
       echo "\$b = $b<br>";
-
-      echo "\$aと\$bのうち最大値は" . max_number($a, $b) . "です。";
+      
+      function max_number ($n1,$n2){
+        if ($n1>$n2){
+          echo "\$n1と\$n2のうち最大値は". $n1 ."です";             
+          }else {
+          echo "\$aと\$n2のうち最大値は". $n2 ."です"; 
+        }
+      }
+      max_number($a, $b)
     ?>  
   </body>
 </html>
